a6c10809f4807151a991bccd40ef66cb1a7eec68
zakarialoulida <144822861+zakarialoulida@users.noreply.github.com>
2023-09-16 09:51:15 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char T[6];
+    T[0]='A';
+    T[1]='O';
+    T[2]='U';
+    T[3]='Y';
+    T[4]='E';
+    T[5]='I';
+    for(int i=0;i<6;i++)
+
+        printf("la valeur de T[%d] =%c\n",i,T[i]);
+    return 0;
+}
