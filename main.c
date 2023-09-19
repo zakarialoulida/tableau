fd815f7532517fa98cf86da01e721322167a8985
zakarialoulida <144822861+zakarialoulida@users.noreply.github.com>
2023-09-19 18:06:20 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 int main()
 {
     char t[]={"Quel sinistre mot !"};
@@ -9,30 +9,38 @@ int main()
        {
         i++;
        }
-printf("[%d]=%c\n",i,t[i]);
+printf("[%d]=%c\n",i+1,t[i]);
 int j=0;
 while(t[j]!='s')
        {
         j++;
        }
-       printf("[%d]=%c\n",j,t[j]);
+       printf("[%d]=%c\n",j+1,t[j]);
        char cmd;
        cmd=t[i];
     t[i]=t[j];
     t[j]=cmd;
    // for(int i=0;t[i]!=\0;i++){
-        printf("%s",t);
-        char lettre,
+        printf("%s\n",t);
+        char lettre;
 
 
-        printf("entrer une lettre");
-        scanf("%s",&lettre);
-        for(int k=0;t!=0;k++){
-        if (t==lettre)
-            {printf("sa postion %d",k);
-            break;}
-            else
-         {printf("elle est absente");}}
+    printf("entrer une lettre:\n");
+        scanf("%c",&lettre);//gets(c);
+          printf("%c",lettre);
+       /* while(t[i]==)
+       {
+        i++;
+       }
+       printf("[%d]=%c\n",i+1,t[i]);*/
+       int a=-1,k =0;
+        for(k=0;k<20;k++){
+
+        if (t[k]==lettre)
+            a=k;
+            }
+if(a!=-1)
+            printf("%d",a);
 
     return 0;
 }
